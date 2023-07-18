c5b04817ecd5e6027492eda0822fc4de8d346755
jidekipro6 <55932100+jidekipro6@users.noreply.github.com>
2023-07-18 01:48:14 +0000

    avance5

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -4,19 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+ 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.PathVariable;
+ 
 // imports for use List, Map, String and Object
-import java.util.List;
-import java.util.Map;
+ 
+ 
 import java.lang.String;
-import java.lang.Object;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @Controller	// This means that this class is a Controller
 @RequestMapping(path="/") // This means URL's start with /demo (after Application path)
