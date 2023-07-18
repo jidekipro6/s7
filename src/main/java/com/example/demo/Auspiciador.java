8ac02cd14067f682a9a40e5f26b264063fd8d4ec
jidekipro6 <55932100+jidekipro6@users.noreply.github.com>
2023-07-18 16:17:42 +0000

    AVANCE13

--- src/main/java/com/example/demo/Auspiciador.java
+++ src/main/java/com/example/demo/Auspiciador.java
@@ -1,14 +1,13 @@
 package com.example.demo;
 
 import org.hibernate.mapping.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
- import jakarta.persistence.Table;
-  import jakarta.persistence.OneToMany;
- import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -20,9 +19,7 @@ public class Auspiciador {
 	private Integer id;
     private String nombre ;
     private Integer monto;
-
-    
-    private Set<Artista>artista= new HashSet<Artista>( );
+private Set<Artista> artista= new HashSet<Artista>();
 
     public Auspiciador() {
     }
